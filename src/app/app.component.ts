2c05b2392fc04621c2c6e0fc05479f5b4a6dbfb3
Ugyen Samdrup <ugyensamdrupdorji@gmail.com>
2022-10-31 19:26:30 +0600

    BG music

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FPL';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      (document.getElementById('abc') as any).play();
+    }, 200);
+  }
+
 }
